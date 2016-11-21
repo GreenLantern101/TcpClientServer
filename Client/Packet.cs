4ba2f37ccf08afeea15c25962a85b8e9adb64de8
GreenLantern101 <arctangent20@yahoo.com>
2016-11-21 11:21:34 -0600

    init

---
+++ Client/Packet.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using Newtonsoft.Json;
+
+namespace Client
+{
+	public class Packet
+	{
+		[JsonProperty("command")]
+		public string Command { get; set; }
+
+		[JsonProperty("message")]
+		public string Message { get; set; }
+
+		// Makes a packet
+		public Packet(string command = "", string message = "")
+		{
+			Command = command;
+			Message = message;
+		}
+
+		public override string ToString()
+		{
+			return string.Format(
+				"[Packet:\n" +
+				"  Command=`{0}`\n" +
+				"  Message=`{1}`]",
+				Command, Message);
+		}
+
+		// Serialize to Json --> TODO: put at end of constructor?
+		public string ToJson()
+		{
+			return JsonConvert.SerializeObject(this);
+		}
+
+		// Deserialize
+		public static Packet FromJson(string jsonData)
+		{
+			return JsonConvert.DeserializeObject<Packet>(jsonData);
+		}
+	}
+	
+	//PRE-PROCESSING needed:
+	/*Call the ToJson() method on the Packet to get it as a string.  
+	Then after that, encode it (in UTF-8) into a byte array
+	Get how many bytes are in that first byte array, 
+	then encode that number as an unsigned 16 bit integer into a second byte array.  
+	The resulting array should be exactly two bytes long
+	Join those two arrays into a new one with the "length buffer," ahead of the "JSON buffer,"
+	*/
+
+	
+	
+}
---
+++ Server/IsClientDisconnected.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsyncMultithreadClientServer
+{
+	/// <summary>
+	/// Description of IsClientDisconnected.
+	/// </summary>
+	public static class IsClientDisconnected
+	{
+		// Checks if a socket has disconnected
+		// Adapted from -- http://stackoverflow.com/questions/722240/instantly-detect-client-disconnection-from-server-socket
+		private static bool _isDisconnected(TcpClient client)
+		{
+			try {
+				Socket s = client.Client;
+				return s.Poll(10 * 1000, SelectMode.SelectRead) && (s.Available == 0);
+			} catch (SocketException se) {
+				// We got a socket error, assume it's disconnected
+				//throw(se);
+				return true;
+			}
+		}
+	}
+}
